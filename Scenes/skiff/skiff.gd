cf69e721708723ceb5a5b9700e5304655e5c67fe
PauloGamarra <paulogamarralessa@gmail.com>
2021-04-22 18:03:45 -0300

    finished greed and wrath levels

---
+++ Scenes/skiff/skiff.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+onready var timer = $MovingTimer
+onready var animation_player=$AnimationPlayer
+
+onready var reset_position = global_position
+
+var velocity = Vector2(0,0)
+
+
+
+export var reset_time : float = 3.0
+
+var  going_down : bool = false
+
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	move_and_slide(velocity, Vector2.UP, true)
+	self.transform.origin.y = 0
+	for i in range(get_slide_count()):
+		var collision = get_slide_collision(i)
+		print(get_slide_collision(i).collider.get_name())
+		if(get_slide_collision(i).collider.get_name().find("Player") != -1):
+			velocity = Vector2(-50,0)
+			collision.collider.move_and_slide(velocity, Vector2.UP, true)
+			
+		
