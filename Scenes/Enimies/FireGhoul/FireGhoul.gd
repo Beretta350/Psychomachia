5bc8ab02fe7fa5aaa92f7a8639d9c3e6b3a80e65
Beretta30 <beretta.gabriel@hotmail.com>
2021-05-18 20:54:30 -0300

    Added fire ghoul enemy

---
+++ Scenes/Enimies/FireGhoul/FireGhoul.gd
@@ -0,0 +1,46 @@
+extends KinematicBody2D
+
+const GRAVITY = 60
+const SPEED = 150
+const SPEED_ATTACK = 200
+
+var velocity = Vector2()
+var direction = 1
+var hit = false
+
+onready var attack_timer = $Timers/AttackTimer
+onready var animations = $Body/Animation/AnimationPlayer
+onready var object_body = $Body
+
+var player = null
+
+func _ready():
+	animations.play("walk")
+	add_to_group("enemies") # Replace with function body.
+	
+func _physics_process(delta):
+	velocity.x = SPEED * direction
+	
+	if (player != null): 
+		if (player.global_position - global_position).length() < 300:
+			animations.play("attack")
+			velocity.x = SPEED_ATTACK * direction
+			if player.global_position.x - global_position.x > 0:
+				direction = 1
+			else:
+				direction = -1	
+
+	if direction == 1:
+		object_body.scale.x = -1
+	else:
+		object_body.scale.x = 1
+
+	velocity += Vector2.DOWN * GRAVITY
+
+	velocity = move_and_slide(velocity, Vector2.UP, true)	
+	
+	if is_on_wall() and (player.global_position - global_position).length() >= 300:
+		direction = direction * -1
+
+func set_player(p):
+	player = p
