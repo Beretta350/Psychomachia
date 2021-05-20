66a3f5227b3ccc833055128512f31252f72131e2
PauloGamarra <paulogamarralessa@gmail.com>
2021-05-20 19:42:27 -0300

    implemented lust boss

--- Scenes/Boss/Lust/Lust.gd
+++ Scenes/Boss/Lust/Lust.gd
@@ -1,36 +1,58 @@
 extends KinematicBody2D
 
-onready var anim : AnimatedSprite = $AnimatedSprite
-onready var timer = $WalkingTimer
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var velocity = Vector2(100,0)
+const GRAVITY = 60
+const SPEED = 70
 
+var velocity = Vector2()
+var direction = 1
+var hit = false
 
-# Called when the node enters the scene tree for the first time.
+onready var attack_timer = $Timers/AttackTimer
+onready var animations = $Body/Animation/AnimationPlayer
+onready var object_body = $Body
+
+var player = null
+
 func _ready():
-	pass # Replace with function body.
-
-func _physics_process(delta):
-	move()
-
-
-func move():
-	move_and_slide(velocity, Vector2.UP, true)
+	add_to_group("enemies") # Replace with function body.
 	
-
+func _physics_process(delta):
+	
 		
+	if hit and attack_timer.is_stopped():
+		animations.play("attack")
+		velocity.x = 0
+		attack_timer.start()
+	elif (hit == false) and attack_timer.is_stopped():
+		if player == null:
+			return
+		
+		if player.global_position.x - global_position.x > 0:
+			direction = 1
+		else:
+			direction = -1
+		
+		if direction == 1:
+			object_body.scale.x = 1
+		else:
+			object_body.scale.x = -1
+			
+		velocity.x = SPEED * direction
+				
+		animations.play("walk")
+	
+	velocity += Vector2.DOWN * GRAVITY
+	
+	if (direction == 1 and global_position.x <612) or (direction == -1 and global_position.x > 371):
+		velocity = move_and_slide(velocity, Vector2.UP, true)
 
-func _on_WalkingTimer_timeout():
-	velocity.x=-velocity.x
-	if anim.flip_h==true:
-		anim.flip_h=false
-	else:
-		anim.flip_h=true
+func set_player(p):
+	player = p
 
 
+func _on_AttackTimer_timeout():
+	hit = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_HitArea_body_entered(body):
+	if body.name == "Player":
+		hit = true
