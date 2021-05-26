5bf22cd5662d08176cac045feb1a27f1861c1f6f
Beretta30 <beretta.gabriel@hotmail.com>
2021-05-26 20:58:00 -0300

    Improved arrow to vanish when hit enemy and wall

--- arrow.gd
+++ arrow.gd
@@ -10,3 +10,12 @@ func _physics_process(delta):
 func _ready():
 	if speed < 0:
 		get_node( "Sprite" ).set_flip_h( true )
+
+func _on_Area2D_body_entered(body):
+	if not "Player" in body.name:
+		queue_free()
+
+
+func _on_Area2D_area_entered(area):
+	if "Hurt" in area.name and not "Player" in area.name:
+		queue_free()
