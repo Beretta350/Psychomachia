38e51a829e7c44033449d788d265ffae5748c5ff
PauloGamarra <paulogamarralessa@gmail.com>
2021-05-26 16:34:48 -0300

    tuned cerberus boss

--- Scenes/Boss/Cerberus/Cerberus.gd
+++ Scenes/Boss/Cerberus/Cerberus.gd
@@ -1,11 +1,11 @@
 extends KinematicBody2D
 
 onready var anim : AnimatedSprite = $AnimatedSprite
-
+onready var object_body : Node2D = $Body
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var velocity = Vector2(150,0)
+var velocity = Vector2(100,0)
 var triggered = false
 var player = null
 var original_position = null
@@ -28,9 +28,15 @@ func set_player(p):
 	player = p
 
 func move():
+	if global_position.x < 388:
+		return
+	
 	var vec_to_player = player.global_position - global_position
 	
-	
+	if vec_to_player.x > 0:
+		object_body.scale.x = -1
+	if vec_to_player.x < 0:
+		object_body.scale.x = 1
 	
 	move_and_slide(vec_to_player.normalized() * velocity, Vector2.UP, true)
 
