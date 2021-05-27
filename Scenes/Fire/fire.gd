613f861b81142b3350e292cd229cb224e228e382
Bruna <bruna.chiochetta@gmail.com>
2021-05-27 20:25:06 -0300

    fixed fire bug with arrows

--- Scenes/Fire/fire.gd
+++ Scenes/Fire/fire.gd
@@ -1,11 +1,6 @@
 extends Area2D
 
-func _on_Fire_area_entered(area):
-	
-	get_tree().reload_current_scene() 
-
-	# Replace with function body.
-
 
 func _on_Fire_body_entered(body):
-	pass
+	if body.name=="Player":
+		get_tree().reload_current_scene() 
