49c7ccffb18cdfcaeeb935112f51328532b1af3c
PauloGamarra <paulogamarralessa@gmail.com>
2021-05-27 21:15:21 -0300

    implemented wrath bat enemies

--- Scenes/Enimies/Bat/Bat.gd
+++ Scenes/Enimies/Bat/Bat.gd
@@ -1,69 +1,121 @@
 extends KinematicBody2D
 
 const GRAVITY = 60
-var velocity = Vector2()
+const MOVE_SPEED = 5000
 
 onready var animations : AnimationPlayer = $Body/Animation/AnimationPlayer
 onready var object_body : Node2D = $Body
 
+export var vertical : bool = true
+export var xtrigger : float = 0.0
+
 var attacking = false
 var player = null
+var skiff = null
 var direction = 1
 var life = 100
 
-onready var lavaBallCD : Timer = $LavaBallCD
+var objective = null
 
-var plLavaball := preload("res://Scenes/Enimies/FireWizard/lavaball.tscn")
+
+var column = randi()%5+1
+var row = randi()%3+1
+onready var attack_timer : Timer = $AttackTimer
+
+var plProjectile := preload("res://Scenes/Enimies/Bat/BatProjectile.tscn")
 
 func _ready():
-	animations.play("idle")
+	animations.play("walk")
 	add_to_group("enemies")
 
 func _physics_process(delta):
-	if life <= 0:
-		animations.play("death")
-	else:
-		if player != null:
-			if player.global_position.x - global_position.x > 0:
-				direction = 1
-			else:
-				direction = -1
-			
-			if direction == 1:
-				object_body.scale.x = -1
-			else:
-				object_body.scale.x = 1
-				
-			if attacking and lavaBallCD.is_stopped():
-				var lavaball := plLavaball.instance()
-				lavaball.position = position + Vector2(0,-30)
-				lavaball.player = player
-				get_tree().current_scene.add_child(lavaball)
-				lavaBallCD.start()
+	
+	if xtrigger < global_position.x:
+		return
+	
+	if player != null and skiff != null:
 		
-		velocity += Vector2.DOWN * GRAVITY
-		velocity = move_and_slide(velocity, Vector2.UP, true)
+		if vertical == true:
+			if attack_timer.is_stopped() and attacking:
+				var projectile := plProjectile.instance()
+				projectile.position = global_position 
+				projectile.direction = Vector2(0,1)
+				projectile.skiff = skiff
+				projectile.fixedx = global_position.x - skiff.global_position.x
+				get_tree().current_scene.add_child(projectile)
+				attacking = false
+			
+			if attack_timer.is_stopped():
+				animations.play("walk")
+				
+				if column == 1:
+					objective = skiff.global_position + Vector2(50,-170)
+				elif column == 2:
+					objective = skiff.global_position + Vector2(-50,-170)
+				elif column == 3:
+					objective = skiff.global_position + Vector2(-25,-170)
+				elif column == 4:
+					objective = skiff.global_position + Vector2(25,-170)
+				elif column == 5:
+					objective = skiff.global_position + Vector2(0,-170)
+				
+				var vec_to_objective = objective - global_position
+				
+				if vec_to_objective.x > 0:
+					object_body.scale.x = 1
+				if vec_to_objective.x < 0:
+					object_body.scale.x = -1
+				
+				if vec_to_objective.length() < 1:
+					column = randi()%5+1
+					attacking = true
+					attack_timer.start()
+					
+				move_and_slide(vec_to_objective.normalized() * MOVE_SPEED  * delta)
+			else:
+				animations.play("attack")
+		
+		else:
+			if attack_timer.is_stopped() and attacking:
+				var projectile := plProjectile.instance()
+				projectile.position = global_position 
+				projectile.direction = Vector2(-1,0)
+				projectile.skiff = skiff
+				projectile.fixedy = global_position.y - skiff.global_position.y
+				get_tree().current_scene.add_child(projectile)
+				attacking = false
+			
+			if attack_timer.is_stopped():
+				animations.play("walk")
+				
+				if row == 1:
+					objective = skiff.global_position + Vector2(150,-55)
+				elif row == 2:
+					objective = skiff.global_position + Vector2(150,-35)
+				elif row == 3:
+					objective = skiff.global_position + Vector2(150,-15)
+				
+				var vec_to_objective = objective - global_position
+				
+				if vec_to_objective.x > 0:
+					object_body.scale.x = 1
+				if vec_to_objective.x < 0:
+					object_body.scale.x = -1
+				
+				if vec_to_objective.length() < 1:
+					row = randi()%3+1
+					attacking = true
+					attack_timer.start()
+					
+				move_and_slide(vec_to_objective.normalized() * MOVE_SPEED  * delta)
+			else:
+				animations.play("attack")
+		
+		
 	
 
 func set_player(p):
 	player = p
-
-func _on_HitArea_body_entered(body):
-	if body.name == "Player":
-		animations.play("attack")
-		attacking = true
-
-func _on_HitArea_body_exited(body):
-	if body.name == "Player":
-		animations.play("idle")
-		attacking = false
-
-func _on_HurtArea_area_entered(area):
-	if "PlayerLightDamage" in area.name:
-		life -= 30
-	elif "PlayerHeavyDamage" in area.name:
-		life -= 50
-
-func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == "death":
-		queue_free()
+	
+func set_skiff(s):
+	skiff = s
---
+++ Scenes/Enimies/Bat/BatProjectile.gd
@@ -0,0 +1,47 @@
+extends KinematicBody2D
+
+const MOVE_SPEED = 200
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+onready var explosionTimer : Timer = $ExplosionTimer
+onready var invincible_timer : Timer = $InvincibleTimer
+
+var exploding = false
+var direction = Vector2(0,0)
+
+var fixedx = null
+var fixedy = null
+
+var skiff = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	move_and_collide(direction.normalized() * MOVE_SPEED  * delta)
+	
+	if fixedx != null:
+		global_position.x = skiff.global_position.x + fixedx 
+	
+	if exploding == true and explosionTimer.is_stopped():
+		self.kill()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func kill():
+	queue_free()
+
+func _on_HitDamage_body_entered(body):
+	if invincible_timer.is_stopped():
+		print('explodir')
+		if exploding == false:
+			exploding = true
+			explosionTimer.start()
